4506cb5b80dca31e165fbedc757db8ff2f0eba93
blubyu <ramida470@gmail.com>
2023-01-19 14:24:28 +0700

    g

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,77 +1,118 @@
 <template>
-    <div></div>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-.v-application {
-    font-family: 'Quicksand', sans-serif;
-}
-.v-toolbar__title {
-    text-align: center;
-    width: 100%;
-    font-weight: bold;
-}
-.v-application .primary {
-  background-color: #C397FF !important;
-  border-color: #C397FF !important;
-}
-.bold {
-  font-weight: bold;
-}
-.uppercase {
-  text-transform: uppercase;
-}
-.text-primary {
-  color: #C397FF !important;
-}
-.text-disabled {
-  color: #888EA0 !important;
-}
-.text-main {
-  font-size: 20px;
-}
-.text-title {
-  font-size: 20px;
-  font-weight: bold;
-}
-.w-100 {
-  width: 100%;
-}
-.my-btn {
-  font-size: 20px !important;
-  text-transform: none !important;
-  height: auto !important;
-  padding: 10px 0 !important;
-  font-weight: bold;
-  min-height: 50px;
-  &.outlined {
-    border: 1px solid #C397FF;
-    border-radius: 28px;
-    line-height: normal;
-    letter-spacing: 0.0892857143em;
-    text-indent: 0.0892857143em;
-    user-select: none;
-    white-space: nowrap;
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
   }
 }
-.my-btn-disabled {
-  font-size: 20px !important;
-  text-transform: none !important;
-  height: auto !important;
-  padding: 10px 0 !important;
-  font-weight: bold;
-  min-height: 50px;
-  &.outlined {
-    border: 1px solid #888EA0;
-    border-radius: 28px;
-    line-height: normal;
-    letter-spacing: 0.0892857143em;
-    text-indent: 0.0892857143em;
-    user-select: none;
-    white-space: nowrap;
-  }
-}
-.set-padding {
-  padding: 0 10px;
-}
-</style>
+</script>
