31b9b986b908021bf6991ab5bee1e4fc4bc2649c
blubyu <ramida470@gmail.com>
2023-01-19 19:05:34 +0700

    test

--- src/views/connect/index.vue
+++ src/views/connect/index.vue
@@ -54,7 +54,23 @@ const lineID = ref([])
 </script>
 
 <script>
+import liff from '@line/liff';
+
+liff.init({
+    liffId: '1234567890-AbcdEfgh', // Use own liffId
+});
+
 export default {
+    mounted() {
+        liff.init({
+            liffId: '1657670230-Jo7GP1Mv', //BLUEZO Event Connect
+        })
+        liff.ready.then(() => {
+            liff.getProfile().then(profile => {
+                alert(profile)
+            })
+        });
+    },
     methods: {
         connect() {
             this.$router.push('/connect-done') 
