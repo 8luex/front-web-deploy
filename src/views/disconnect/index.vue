02d56124a2a0879c81187d9faab56f5607853652
blubyu <ramida470@gmail.com>
2023-01-29 04:53:58 +0700

    Update index.vue

--- src/views/disconnect/index.vue
+++ src/views/disconnect/index.vue
@@ -14,13 +14,13 @@
                 </v-col>
                 <v-col cols="12" class="text-center">
                     <div class="mt-1" style="font-size: 18px;">
-                        {{ item.studentID }}
+                        {{ items.studentID }}
                     </div>
                     <div class="mt-1" style="font-size: 18px;">
-                        {{ item.fname }} {{ item.lname }}
+                        {{ items.fname }} {{ item.lname }}
                     </div>
                     <div class="mt-1" style="font-size: 18px;">
-                        {{ item.faculty }}
+                        {{ items.faculty }}
                     </div>
                 </v-col>
                 <v-col cols="12" class="text-center pl-10 pr-10">
@@ -39,28 +39,63 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+// const items = ref([])
 
-const items = ref([])
-
-fetch('https://apricot-binturong-kit.cyclic.app/studentgetconnectcheck/'+getLine.userId.value)
-.then(res => res.json())
-.then((result) => {
-  items.value = result
-  console.log(result)
-})
-
-//<router-link to="/"></router-link>
+// fetch('https://apricot-binturong-kit.cyclic.app/studentgetconnectcheck/'+getLine.userId.value)
+// .then(res => res.json())
+// .then((result) => {
+//   items.value = result
+//   console.log(result)
+// })
 </script>
 
 <script>
+import { computed, ref } from 'vue';
+import liff from '@line/liff';
+import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+
 export default {
-    // setup() {
-    //     const line = ref([])
-    //     return {
-    //         line
-    //     }
-    // },
+    setup() {
+        const store = useStore();
+        const lineID = computed(() => store.getters.getLine.userId);
+        const items = ref([])
+
+        const getconnect = () => {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "lineID": lineID.value
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch("https://apricot-binturong-kit.cyclic.app/studentdisconnectcheck", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                if(result.message === 'already connected') {
+                    items.value = result.line
+                } else if(result.message === 'not yet connected') {
+                    alert('ยังไม่ได้เชื่อมโยงบัญชี')
+                } else {
+                    alert(JSON.stringify(result))
+                }
+            })
+            .catch(error => console.log('error', error));
+        }
+
+        return {
+            store, lineID,
+            items,
+            getconnect
+        }
+    },
     mounted() {
         liff.init({
             liffId: '1657670230-mPD2o50K', //BLUEZO Event Disconnect
