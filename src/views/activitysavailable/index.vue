4449a2065d3d4ed027e11697accf5cf077605b02
blubyu <ramida470@gmail.com>
2023-02-04 05:50:52 +0700

    Update index.vue

--- src/views/activitysavailable/index.vue
+++ src/views/activitysavailable/index.vue
@@ -47,69 +47,17 @@
     </div>
 </template>
 
-<script setup>
-import { computed, ref } from 'vue';
-import { useStore } from 'vuex'
-const store = useStore()
-//const lineID = ref('')
-const items = ref([])
-const getconnect=(lineID)=> {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
 
-    var raw = JSON.stringify({
-        "lineID": lineID
-    });
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    fetch("https://apricot-binturong-kit.cyclic.app/studentdisconnectcheck", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        if(result.message === 'already connected') {
-            console.log(result)//Test
-            getactivitysavailable(result.line[0].studentID);
-        } else if(result.message === 'not yet connected') {
-            alert('ยังไม่ได้เชื่อมโยงบัญชี'+lineID)//
-        } else {
-            alert(JSON.stringify(result))
-        }
-    })
-    .catch(error => console.log('error', error));
-}
-const getactivitysavailable=(studentID)=> {
-    fetch('https://apricot-binturong-kit.cyclic.app/activitysavailable/'+studentID)
-    .then(res => res.json())
-    .then((resultact) => {
-        if(resultact.status === 'error') {
-            alert(JSON.stringify(resultact))
-        } else if(resultact.message === 'no activitys available') {
-            console.log(resultact)
-        } else {
-            //alert(JSON.stringify(resultact))
-            //this.items = resultact
-            items.value = resultact
-            console.log(resultact)
-        }
-    })
-}
-//getconnect(lineID);
-</script>
 
 <script>
 import Card from '@/components/Card.vue'
-
+import { computed, ref } from 'vue';
 import liff from '@line/liff';
-
+import { useStore } from 'vuex'
 import { useRouter, useRoute } from 'vue-router'
 
-//const items = ref([])
-//const itemsline = ref([])
+const items = ref([])
+const itemsline = ref([])
 
 export default {
     name: 'activity',
@@ -144,10 +92,9 @@ export default {
             }       
             liff.getProfile().then(profile => {
                 console.log(profile)
-                //this.lineID.value = profile.userId;
+                //this.lineID = profile.lineID;
                 this.$store.dispatch('setLine', profile); //try
-                this.getconnect(profile.userId);
-                //this.getconnect();
+                this.getconnect();
                 //this.isDone();
             })
         });
@@ -162,7 +109,52 @@ export default {
             this.isShowDialog = true
             this.dialog= item
         },
-        
+        getconnect() {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+                "lineID": this.$store.getters.getLine.userId
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch("https://apricot-binturong-kit.cyclic.app/studentdisconnectcheck", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                if(result.message === 'already connected') {
+                    itemsline.value = result.line[0] //nomatter
+                    console.log(result)//Test
+                    this.getactivitysavailable(result.line[0].studentID);
+                } else if(result.message === 'not yet connected') {
+                    alert('ยังไม่ได้เชื่อมโยงบัญชี')
+                } else {
+                    alert(JSON.stringify(result))
+                }
+            })
+            .catch(error => console.log('error', error));
+        },
+        getactivitysavailable(studentID) {
+            fetch('https://apricot-binturong-kit.cyclic.app/activitysavailable/'+studentID)
+            .then(res => res.json())
+            .then((resultact) => {
+                if(resultact.status === 'error') {
+                    alert(JSON.stringify(resultact))
+                } else if(resultact.message === 'no activitys available') {
+                    console.log(resultact)
+                } else {
+                    //alert(JSON.stringify(resultact))
+                    //this.items = resultact
+                    items.value = resultact
+                    console.log(resultact)
+                }
+            })
+        }
     }
 }
 </script>
