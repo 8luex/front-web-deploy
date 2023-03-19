8c2d9cad5dccdbb31a611838a74750a3c8071ff6
blubyu <ramida470@gmail.com>
2023-03-19 22:26:52 +0700

    tt

---
+++ src/views/teacher/created/test.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <v-app-bar color="teal-accent-3" dense flat dark>
+            <v-toolbar-title style="text-align: center;color: white;">กิจกรรมที่สร้าง</v-toolbar-title>
+        </v-app-bar>
+        <v-container class="pt-0 pb-0">
+            <v-row>
+                <v-col cols="12">
+                    <v-btn color="teal-accent-3" text @click="viewTicket()">ลงทะเบียน</v-btn>
+                </v-col>
+                <v-col cols="12" class="text-center">
+                    <div class="mt-2 text-caption text-disabled">
+                        &copy;Scholarship Activity 2023
+                    </div>
+                </v-col>
+            </v-row>
+            <v-dialog v-model="isShowDialog" max-width="290">
+                <v-card class="dialog-card">
+                    <v-img cover class="white--text align-end" height="200px"></v-img>
+                    <v-card-title class="text-h6">
+                        cbnvbnc
+                    </v-card-title>
+                    <v-card-text>
+                        <p class="text-caption text-disabled">cvbn</p>
+                        <p>ผู้สร้างกิจกรรม: cvbnvbn</p>
+                        <p>bncvbn</p>
+                        <p class="detail">รายละเอียดกิจกรรม: vbcnvbncfffffffffffffffffff fgggggggg fgfgf gfgf gf gf fgfgf gffgfgf gfg f gfg gfgfgfgfgfgfg fggfgfgfg fggfgfgfg gf gfg fgggfgf gf gf fgfgf gffgfgf gfg f gfg gfgfgfgfgfgfg fggfgfgfg fggfgfgfg gf gfg fgggf gf fgfgf gffgfgf gfg f gfg gfgfgfgfgfgfg fggfgfgfg fggfgfgfg gf gfg fggf g fgfgfgfgfg gf</p>
+                        <p>วันที่: cbncvb</p>
+                        <p>เวลา: cbnv</p>
+                        <p>สถานที่: cbncvb</p>
+                        <p>ชั่วโมงกิจกรรมที่จะได้รับ: cbvnvb</p>
+                        <v-btn variant="flat" rounded color="teal-accent-3" style="color: white !important;" class="w-100 mt-2" @click="viewTicket()">
+                            <v-icon size="large">mdi-line-scan</v-icon>Scan to check
+                        </v-btn>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="grey" text @click="isShowDialog = false">ยกเลิก</v-btn>
+                        <v-btn color="teal-accent-3" text @click="isShowDialog = false">ลงทะเบียน</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-container>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'myactivityenroll',
+    components: {
+    },
+    data () {
+        return {
+            isShowDialog: false,
+        }
+    },
+    setup() {
+        
+    },
+    mounted() {
+        
+    },
+    computed: {
+    },
+    methods: {
+        viewTicket() {
+            this.isShowDialog = true
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
