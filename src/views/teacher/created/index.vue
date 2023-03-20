37f814aca281c84177f1916720d86a1081b16183
blubyu <ramida470@gmail.com>
2023-03-21 00:32:38 +0700

    Revert "Update index.vue"
    This reverts commit 4ff5bcc4069e43b135d1289355606cf760dcde0e.

--- src/views/teacher/created/index.vue
+++ src/views/teacher/created/index.vue
@@ -161,12 +161,7 @@ export default {
         },
         scan() {
             liff.scanCodeV2().then(result => { //ios
-                // alert(result.value);
-                if(result.value=="test") {
-                    this.$router.push('create-done');
-                } else {
-                    alert(result.value);
-                }
+                alert(result.value);
             }).catch(e => alert(e))
         },
     }
