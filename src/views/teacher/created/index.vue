15f6a6d23a66112b474a77057aeb32763e411139
blubyu <ramida470@gmail.com>
2023-05-02 17:41:48 +0700

    Update index.vue

--- src/views/teacher/created/index.vue
+++ src/views/teacher/created/index.vue
@@ -231,8 +231,8 @@ export default {
         viewTicket(item) {
             this.isShowDialog = true
             this.dialog= item
-            console.log(item[0][0])
-            this.getwhoenroll(item[0][0])
+            console.log(this.dialog.activityID.value)
+            this.getwhoenroll(this.dialog.activityID)
         },
         scan() {
             liff.scanCodeV2().then(result => {
