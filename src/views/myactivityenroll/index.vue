82cd2dbb514f769e5fc589df3b5c7e3e0d9dea04
blubyu <ramida470@gmail.com>
2023-05-05 19:13:50 +0700

    Update index.vue

--- src/views/myactivityenroll/index.vue
+++ src/views/myactivityenroll/index.vue
@@ -34,14 +34,12 @@
                         <p>เวลา: {{ dialog.timeStart }}-{{ dialog.timeEnd }}</p>
                         <p>สถานที่: {{ dialog.location }}</p>
                         <p>ชั่วโมงกิจกรรมที่จะได้รับ: {{ dialog.hoursToReceive }}</p>
-                        <div style="display: block; margin-left: auto; margin-right: auto;">
-                            <VueQrcode
-                                v-bind:value="qrValue"
-                                v-bind:color="qrColor"
-                                v-bind:type="qrType"
-                                v-bind:errorCorrectionLevel="correctionLevel"
-                            />
-                        </div>
+                        <VueQrcode style="display: block; margin-left: auto; margin-right: auto;"
+                            v-bind:value="qrValue"
+                            v-bind:color="qrColor"
+                            v-bind:type="qrType"
+                            v-bind:errorCorrectionLevel="correctionLevel"
+                        />
                         <p class="text-caption text-disabled text-center">แสดง qr code นี้กับเจ้าของกิจกรรม</p>
                     </v-card-text>
                     <v-card-actions>
